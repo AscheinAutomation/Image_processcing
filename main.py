b642d3d6caf1397202e10f02e856fa17babd2464
Võ Ngọc Trác <114114519+AscheinAutomation@users.noreply.github.com>
2023-10-08 21:43:10 +0700

    Update main.py
    nãy coi lại code của thầy mới thấy là k cần đặt tên biến dài, tỉ lệ fx fy thì cứ thêm vào trong lệnh resize luôn =)))

--- main.py
+++ main.py
@@ -2,12 +2,11 @@ import cv2
 
 image = cv2.imread('TTD_1014.JPG')
 
-scale_factor_x = 0.15
-scale_factor_y = 0.15
+# scale_factor_x = 0.15
+# scale_factor_y = 0.15
 
-resized_image = cv2.resize(image, None, fx=scale_factor_x, fy=scale_factor_y)
+resized_image = cv2.resize(image, None, fx=0.15, fy=0.15)
 
 cv2.imshow('Resized_image',resized_image)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
-
